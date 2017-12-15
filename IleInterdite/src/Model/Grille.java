90615042cc84906603d6afa31021b8fdef747fe2
Daseikay <Daseikay@live.fr>
2017-12-15 11:50:31 +0100

    Coucou O'neal

--- IleInterdite/src/Model/Grille.java
+++ IleInterdite/src/Model/Grille.java
@@ -12,11 +12,25 @@ import java.util.*;
  * @author barrerat
  */
 public class Grille {
-    Tuile[][] tuiles;
-    
-    public Grille() {
-        tuiles = new Tuile[6][6];
+
+    private Tuile[][] tuiles = new Tuile[6][6];
+
+    public Grille(ArrayList<Tuile> tu){
+        for (int j = 0; j < 6; j++){
+            for (int i = 0; i < 6; i++){
+                if ((i == 0 && j == 0) || (i == 1 && j == 0)) || (i == 0 && j == 1) ||                                  // Coin haut gauche
+                        (i == 0 && j == 4) || (i == 0 && j == 5) || (i == 1 & j == 5) ||                                // Coin haut droit
+                        (i == 4 && j == 0) || (i == 5 && j == 0) || (i == 5 & j == 1) ||                                // Coin bas gauche
+                        (i == 4 && j == 5) || (i == 5 && j == 5) || (i == 5 && j = 4)){                                 //Coin bas droit
+
+                    this.tuiles = null;
+
+                }
+
+            }
+        }
     }
+
     
     
     
