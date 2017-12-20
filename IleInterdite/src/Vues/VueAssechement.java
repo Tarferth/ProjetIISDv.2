0e00d13d68d1ae70b7e6240d2846b56b62b67f27
Oneal71 <oneal69@hotmail.fr>
2017-12-20 12:59:37 +0100

    On est dans la merde mais ça peut passer

--- IleInterdite/src/Vues/VueAssechement.java
+++ IleInterdite/src/Vues/VueAssechement.java
@@ -5,10 +5,52 @@
  */
 package Vues;
 
-/**
- *
- * @author avognonm
- */
+import javax.swing.*;
+import java.awt.*;
+
+
 public class VueAssechement {
-    
+
+        private final JFrame windows;
+        private final JPanel mainPanels;
+
+        private final JButton btnretour;
+        private final JPanel panelBoutons ;
+        public VueAssechement (String nomJoueur, String NomAventurier, Color couleur){
+
+            this.windows = new JFrame();
+            windows.setSize(175, 100);
+            //le titre = nom du joueur
+            windows.setTitle(nomJoueur);
+            mainPanels = new JPanel(new BorderLayout());
+            this.windows.add(mainPanels);
+
+            mainPanels.setBackground(new Color(230, 230, 230));
+            mainPanels.setBorder(BorderFactory.createLineBorder(couleur, 2)) ;
+
+
+    //Bouton retour en bas de la fenêtre//
+     this.panelBoutons = new JPanel(new GridLayout(2,2));
+     this.panelBoutons.setOpaque(false);
+     mainPanels.add(this.panelBoutons, BorderLayout.SOUTH);
+
+     this.btnretour = new JButton("Retour") ;
+
+    //=======================================================================
+
+
+     this.windows.setVisible(true);
 }
+
+
+
+    public JButton getBtnretour() {
+        return btnretour;
+    }
+
+    //============================================================================
+
+
+}
+
+
